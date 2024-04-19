92b69a2e8004f7d6a8f6d4d614a679a45e66b28b
MW <mateusz.wasowski2002@gmail.com>
2024-04-19 13:04:30 +0200

    Layout added
    Dashboard layout and logins added, along with routing and controller

--- shop/routes/web.php
+++ shop/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function(){
+
+    Route::get('/dashboard', [AdminController::class, 'dashboard']);
+    route::match(['get','post'], '/login', [AdminController::class, 'login']);
 });
